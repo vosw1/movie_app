2ec5b66f5d0b2c0435ca1a2adc0b5d6c0c5d35b2
vosw1 <vosw1@naver.com>
2024-09-10 17:26:59 +0900

    간단 영화 정보

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>영화 정보</h1>
+  <div v-for="(movie, i) in movies" :key="i">
+    <h3 class="bg-yellow" :style="{ color: movie.titleColor }">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      movies: [
+        {
+          title: "에이리언: 로물루스",
+          year: "2024년 8월 16일",
+          category: "SF, 호러, 액션, 스릴러, 크리처",
+          titleColor: "red"
+        },
+        {
+          title: "트위스터스",
+          year: "2024년 8월 14일",
+          category: "재난, 액션, 모험, 드라마",
+          titleColor: "brown"
+        },
+        {
+          title: "스픽 노 이블",
+          year: "2024년 9월 11일",
+          category: "공포/드라마",
+          titleColor: "grey"
+        },
+        {
+          title: "안녕, 할부지",
+          year: "2024년 9월 4일",
+          category: "다큐멘터리, 애니메이션",
+          titleColor: "green"
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.bg-yellow {
+  background: yellow;
+  padding: 10px;
 }
 </style>
